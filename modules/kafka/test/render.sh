5f3ccf83f5593b015115197ec6508e62f9e11572
zhenik <nikita.zhevnitskiy@gmail.com>
2020-03-09 16:17:29 +0100

    added: zoo and render

--- modules/kafka/test/render.sh
+++ modules/kafka/test/render.sh
@@ -1,1 +1,1 @@
-sudo consul-template -consul-addr=http://172.17.0.1:8500 -once -template ./test0.ctpl:out.txt
+sudo consul-template -consul-addr=http://172.17.0.1:8500 -once -template ./test6.ctpl:out.txt
